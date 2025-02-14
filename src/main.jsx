b351aab78d526cb57ccd793daf7ce19411f3d08b
mohdh34m <68151199+mohdh34m@users.noreply.github.com>
2025-02-14 04:40:46 -0800

    Revert "update: change BrowserRouter to HashRouter"
    This reverts commit 9d01a457acdc8ac405e082a87f8c36d411bd2a3c.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Routes, Route, HashRouter } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 import { UserProvider } from './lib/context/user';
 import { BooksProvider } from "./lib/context/books";
 import './index.css'
@@ -22,7 +22,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <UserProvider>
       <BooksProvider>
-        <HashRouter>
+        <BrowserRouter>
           <Routes>
             <Route index element={<App />} />
             <Route path="/books/:id" element={<BookDetails />} />
@@ -38,7 +38,7 @@ createRoot(document.getElementById('root')).render(
             </Route>
             <Route path='*' element={<NotFound />} />
           </Routes>
-        </HashRouter>
+        </BrowserRouter>
       </BooksProvider>
     </UserProvider>
   </StrictMode>,
